e17ca3a975311a4cc043d1e9528c38040aac5cf3
E208633A <jolan.renoux@etu.univ-nantes.fr>
2021-12-07 14:47:45 +0100

    .

--- src/automaton/AutomatonExporter.java
+++ src/automaton/AutomatonExporter.java
@@ -1,7 +1,24 @@
 package automaton;
 
+import javax.swing.*;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class AutomatonExporter {
 
+
+    public static boolean export(Automaton autom, String file) {
+        try {
+
+            FileWriter writer = new FileWriter(file, true);
+
+            StringBuilder str;
+
+
+
+    }catch (IOException e){
+            return false;
+        }
 
 
 }
